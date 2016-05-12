f55d4b3d0aeadb179e32a4068eb2ead6c633e146
zackwitz <zackwitz@gmail.com>
2016-05-12 12:49:47 -0700

    Opens frame

--- src/Gamestate.java
+++ src/Gamestate.java
@@ -1,4 +1,5 @@
 import java.awt.*;						
+
 import javax.swing.*;
 
 public class Gamestate extends JFrame
@@ -6,7 +7,13 @@ public class Gamestate extends JFrame
 
 	public static void main(String[] args)
 	{
+		Gamestate gs = new Gamestate();
+		gs.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gs.setTitle("MLGerio");				// Optional title
+		gs.setSize(1200, 750);					// Set the width and height of the window
+		gs.setVisible(true);	
 		
 	}
 
+	
 }
