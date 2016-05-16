0c06c849ab50b2b889f308376acc5804f8029dd7
RyanFischbach <RTF@Ryans-MacBook-Air-3.local>
2016-05-16 11:14:24 -0700

    Gamestate Update to Have KeyLIstener/ActionListener

--- src/Gamestate.java
+++ src/Gamestate.java
@@ -1,7 +1,8 @@
-import java.awt.*;			
-import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*; 			
 
-public class Gamestate extends JFrame
+public class Gamestate extends JFrame implements KeyListener, ActionListener
 {
 
 	final static int FRAME_WIDTH = 1200;
@@ -9,21 +10,65 @@ public class Gamestate extends JFrame
 	final static int BLOCK_SIDE = 75;
 	final static int BLOCKS_VERT = FRAME_HEIGHT / BLOCK_SIDE;
 	final static int BLOCKS_HOR = FRAME_WIDTH / BLOCK_SIDE;
+	private static final int DELAY_IN_MILLISEC = 50;
 
+	public Gamestate()
+	{
+		Timer clock = new Timer(DELAY_IN_MILLISEC, this);
+		clock.start();
+	}
+	
 	public static void main(String[] args)
 	{
 		Gamestate gs = new Gamestate();
 		gs.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		gs.setTitle("MLGerio");			 				
 		gs.setSize(FRAME_WIDTH, FRAME_HEIGHT);
-		gs.setVisible(true);	
+		gs.setVisible(true);
+		gs.addKeyListener(gs);
+		
+	}	
+	public void actionPerformed(ActionEvent e)
+	{
+		repaint();
 	}
 
-	public void paint(Graphics g) 		
+	public void keyPressed(KeyEvent e)
+	{
+		int keyCode = e.getKeyCode();
+		if(keyCode == KeyEvent.VK_RIGHT)
+		{
+			// move right
+			// switch model right
+		}
+		else if(keyCode == KeyEvent.VK_LEFT)
+		{
+			//move left
+			// switch model to left
+		}
+		else if(keyCode == KeyEvent.VK_SPACE)
+		{
+			// jump
+		}
+	}
+	
+	public void keyTyped(KeyEvent e)
+	{
+	}
+	
+	public void keyReleased(KeyEvent e)
+	{
+	}
+	
+	public void paint(Graphics g, int start) 		
 	{
 		Level level1 = new Level();
 		boolean [][] bricks = level1.getBricks();
-		paintBricksFrom(g, bricks, 2200);
+		boolean levelEnd = false;
+		while (!levelEnd)
+		{
+			paintBricksFrom(g, bricks, start);
+		}
 	}
 
 
