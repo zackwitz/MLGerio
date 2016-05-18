2a7495b12011109f740aef7e15e80ba5ab657705
zackwitz <zackwitz@gmail.com>
2016-05-18 09:17:37 -0700

    Changed Scott's Character Class

--- src/Character.java
+++ src/Character.java
@@ -1,41 +1,27 @@
 import java.awt.Graphics;
 import java.util.*;
+import javax.swing.*;
 
-public class Merio implements JFrame {
+public class Character implements JFrame {
 
 	private final int SIZE = 75;
-	private final int SPEED = 3;
 	private final int JUMP_STRENGTH = 24;
 	private int accel = 0;
 	private int x;
 	private int y;
 	private boolean jumping = false;
-	private boolean facingLeft = false;
 
-	BufferedImage merioLeft = null;
-	try {
-		merioLeft = ImageIO.read(new File("MerioLeft.png"));
-	} catch (IOException e) {
-	}
-
-	BufferedImage merioRight = null;
-	try {
-		merioRight = ImageIO.read(new File("MerioRight.png"));
-	} catch (IOException e) {
-	}
-
-	public Character (itn initX, int initY)
+	public Character (int initX, int initY)
 	{
 		x = initX;
 		y = initY;
 	}
-	
-	
-	
-	//setters for variables here
-	
-	
 
+	public int getY ()
+	{
+		return y;
+	}
+	
 	public void move ()
 	{
 		accelerate();
@@ -61,17 +47,7 @@ public class Merio implements JFrame {
 		}
 	}
 
-	public void draw (Graphics g)
-	{
-		if (facing left)
-		{
-			g.drawImage(merioLeft, x, y, x+SIZE, y, x, y+SIZE, x+SIZE, y+SIZE, null)
-		}
-		else
-		{
-			g.drawImage(merioRight, x, y, x+SIZE, y, x, y+SIZE, x+SIZE, y+SIZE, null)
-		}
-	}
+	
 
 
 }
