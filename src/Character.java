556772dd11e446cd7660c88b7ae8a11d43de1c5d
ScottMoffatLittle <ScottLittle@Scotts-MacBook-Pro-2.local>
2016-05-18 08:39:02 -0700

    More work on character

--- src/Character.java
+++ src/Character.java
@@ -1,76 +1,77 @@
+import java.awt.Graphics;
 import java.util.*;
 
-public class Character implements JFrame implements KeyListener, ActionListener {
+public class Merio implements JFrame {
 
 	private final int SIZE = 75;
 	private final int SPEED = 3;
-	private final int JUMP_STRENGTH = 10;
+	private final int JUMP_STRENGTH = 24;
 	private int accel = 0;
 	private int x;
 	private int y;
 	private boolean jumping = false;
-	private boolean 
-	
-	
-	public Character (itn initX, int initY, image left, image right)
+	private boolean facingLeft = false;
+
+	BufferedImage merioLeft = null;
+	try {
+		merioLeft = ImageIO.read(new File("MerioLeft.png"));
+	} catch (IOException e) {
+	}
+
+	BufferedImage merioRight = null;
+	try {
+		merioRight = ImageIO.read(new File("MerioRight.png"));
+	} catch (IOException e) {
+	}
+
+	public Character (itn initX, int initY)
 	{
 		x = initX;
 		y = initY;
 	}
 	
+	
+	
+	//setters for variables here
+	
+	
+
 	public void move ()
 	{
 		accelerate();
 		y += accel;
 	}
-	
-	public void keyPressed(KeyEvent e)
+
+	public void jump()
 	{
-		int keyCode = e.getKeyCode();
-		else if (keyCode == KeyEvent.VK_LEFT)
+		if (!jumping)
 		{
-			
-		}
-		else if (keyCode == KeyEvent.VK_RIGHT)
-		{
-			
-		}
-		else if (keyCode == KeyEvent.VK_SPACE)
-		{
-			if (!jumping)
-			{
-				accel = 24;
-			}
+			accel = JUMP_STRENGTH;
 			jumping = true;
 		}
-		repaint();
 	}
-	public void keyTyped(KeyEvent e) {}
-	public void keyReleased(KeyEvent e) {}
-	
-	public void jump()
+
+	public void accelerate()
 	{
 		//need if statement to check for collision
 		//if collided, set jumping to false, accel to 0, and y value to height of block + 75
-		if (!jumping)
-		{
-			accel = 20;
-		}
-		jumping = true;
-	}
-	
-	public void accelerate()
-	{
 		if (jumping)
 		{
 			accel -= 1;
 		}
 	}
-	
+
 	public void draw (Graphics g)
 	{
-		
+		if (facing left)
+		{
+			g.drawImage(merioLeft, x, y, x+SIZE, y, x, y+SIZE, x+SIZE, y+SIZE, null)
+		}
+		else
+		{
+			g.drawImage(merioRight, x, y, x+SIZE, y, x, y+SIZE, x+SIZE, y+SIZE, null)
+		}
 	}
-	
-	
+
+
 }
