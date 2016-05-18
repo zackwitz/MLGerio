105378fd8471c56557ad3c16423625a84b4bcb32
ScottMoffatLittle <ScottLittle@Scotts-MacBook-Pro-2.local>
2016-05-18 08:43:46 -0700

    changed name

--- src/Character.java
+++ src/Character.java
@@ -1,4 +1,75 @@
+import java.awt.Graphics;
+import java.util.*;
 
-public abstract class Character {
+public class Character implements JFrame {
+
+	private final int SIZE = 75;
+	private final int SPEED = 3;
+	private final int JUMP_STRENGTH = 24;
+	private int accel = 0;
+	private int x;
+	private int y;
+	private boolean jumping = false;
+	private boolean facingLeft = false;
+
+	BufferedImage merioLeft = null;
+	try {
+		merioLeft = ImageIO.read(new File("MerioLeft.png"));
+	} catch (IOException e) {
+	}
+
+	BufferedImage merioRight = null;
+	try {
+		merioRight = ImageIO.read(new File("MerioRight.png"));
+	} catch (IOException e) {
+	}
+
+	public Character (itn initX, int initY)
+	{
+		x = initX;
+		y = initY;
+	}
+	
+	
+	
+	
+
+	public void move ()
+	{
+		accelerate();
+		y += accel;
+	}
+
+	public void jump()
+	{
+		if (!jumping)
+		{
+			accel = JUMP_STRENGTH;
+			jumping = true;
+		}
+	}
+
+	public void accelerate()
+	{
+		//need if statement to check for collision
+		//if collided, set jumping to false, accel to 0, and y value to height of block + 75
+		if (jumping)
+		{
+			accel -= 1;
+		}
+	}
+
+	public void draw (Graphics g)
+	{
+		if (facingLeft)
+		{
+			g.drawImage(merioLeft, x, y, x+SIZE, y, x, y+SIZE, x+SIZE, y+SIZE, null)
+		}
+		else
+		{
+			g.drawImage(merioRight, x, y, x+SIZE, y, x, y+SIZE, x+SIZE, y+SIZE, null)
+		}
+	}
+
 
 }
